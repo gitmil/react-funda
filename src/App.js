0f4f5bb4b73cda19c18ca9bd25bbfead755b3125
techsithgit <hemilp@tuplecorp.com>
2019-05-26 23:18:52 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+
+// try inline first
+// object
+// css then
+// convert to scss
+//npm install node-sass
+// how to commnet
+
+const nameStyle = {
+  color: "gray",
+  border: "1px solid gray",
+  width: "300px",
+  paddingTop: "5px",
+  paddingBottom: "5px"
+};
+
+const nameTagStyle = {
+  color: "white",
+  border: "initial"
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{ ...nameStyle, ...nameTagStyle }}>Name List</h1>
+        <h3 style={nameStyle}>Peter</h3>
+        <h3 style={nameStyle}>John</h3>
+        <h3 style={nameStyle}>Jill</h3>
+
+        {/* <h1 className="title">Name List</h1>
+        <h3 className="name">Peter</h3>
+        <h3 className="name">John</h3>
+        <h3 className="name">Jill</h3> */}
       </header>
     </div>
   );
